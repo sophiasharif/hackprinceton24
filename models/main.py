c78819724f27337259d75f21aaaa62c18e134f8d
sophiasharif <57047003+sophiasharif@users.noreply.github.com>
2024-11-08 20:43:37 -0800

    model class

--- models/main.py
+++ models/main.py
@@ -1,4 +1,6 @@
 # Use a pipeline as a high-level helper
+from dataclasses import dataclass
+from enum import Enum
 from transformers import pipeline
 from PIL import Image
 
@@ -9,12 +11,37 @@ MODELS = {
 }
 pipes = {name: pipeline("image-classification", model=model_id) for name, model_id in MODELS.items()}
 
-pipe = pipes["trash1"]
 
-# load image
-image_path = "image1.png"
-image = Image.open(image_path)
+class Model:
+    def __init__(self, model_id, class_to_label_map):
+        self.pipe = pipeline("image-classification", model=model_id)
+        self.class_to_label_map = class_to_label_map
+    
+    def classify(self, image_path):
+        image = Image.open(image_path)
+        results = self.pipe(image)
+        print(results)
+        label = self.class_to_label_map[results[0]["label"]]
+        return label
 
-# classify
-results = pipe(image)
-print(results)
+
+# pipe = pipes["trash1"]
+
+# # load image
+# image_path = "image1.png"
+# image = Image.open(image_path)
+
+# # classify
+# results = pipe(image)
+# print(results)
+
+test = Model(MODELS["trash1"], {
+    'Paper': 0,
+    'Other': 2,
+    'Plastic': 0,
+    'G_M': 0,
+    'Organic': 1
+})
+
+print(test.classify("image2.png"))
+
